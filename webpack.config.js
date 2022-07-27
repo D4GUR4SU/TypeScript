9050319c13e2eb0baa3a174577c22e63c166f33d
Douglas Souza <douglas.barbosa@sensedia.com>
2022-07-27 16:20:42 -0300

    Usando this em function

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/webpack/index.ts',
+  entry: './src/exercicio/exercicio.ts',
   module: {
     rules: [
       {
