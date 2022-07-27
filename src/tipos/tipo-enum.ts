38b29734d9255aa959cdb17d73bebcabd993e0e6
Douglas Souza <douglas.barbosa@sensedia.com>
2022-07-27 10:19:51 -0300

    Tipo enum

---
+++ src/tipos/tipo-enum.ts
@@ -0,0 +1,18 @@
+// estrutura de dados não ordenada para colocar vários tipos de dados
+enum Cores {
+  VERMELHO,
+  AZUL,
+  AMARELO
+}
+
+enum Cores {
+  ROXO = 'ROXO',
+  VERDE = 201,
+  ROSA,
+}
+
+function escolhaCor(cor: Cores): void {
+  console.log(Cores[cor]);
+}
+
+escolhaCor(124345);
